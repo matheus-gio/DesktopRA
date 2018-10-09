c6e51692a13a60759cb38d0b2907793253699bb0
Matheus <matheus.giovanni.lu@gmail.com>
2018-10-09 17:43:05 -0300

    V1.2

--- DesktopRA/MainPage.xaml.cs
+++ DesktopRA/MainPage.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using Newtonsoft.Json;
+using RALIBRARY;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -10,6 +13,7 @@ using System.Text;
 using System.Threading;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -18,6 +22,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -31,6 +36,10 @@ namespace DesktopRA
 
         Boolean fullscreen = false; double canvas_heig,canvas_wid;
         List<Product> produtos = new List<Product>();
+        private double x_first_pick, y_first_pick;
+        private bool linha = false, circulo = false, quadrado = false, retangulo = false, click = false;
+
+        Drawing_Canvas drawingCanvas = new Drawing_Canvas();
 
         public MainPage()
         {
@@ -91,10 +100,101 @@ namespace DesktopRA
 
         }
 
+        private bool first_pick = false;
+
+        private void canvas_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            if (!menu_click)
+            {
+                if (quadrado)
+                {
+                    drawingCanvas.CreateSquare(canvas, e, 10);
+                }
+                else if (retangulo)
+                {
+                    drawingCanvas.Create_Rectangle(canvas, e);
+                }
+                else if (circulo)
+                {
+                    drawingCanvas.CreateCircle(canvas, e, 10);
+                }else if (linha)
+                {
+                    drawingCanvas.CreateLine(canvas, 10, e);
+                }
+            }
+        }
+
+        private void canvas_PointerMoved(object sender, PointerRoutedEventArgs e)
+        {
+            
+        }
+
+
+
+        private void MenuFlyoutItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            linha = true;
+            quadrado = false;
+            circulo = false;
+            retangulo = false;
+        }
+
+        private bool menu_click = false;
+        private void MenuFlyout_Opened(object sender, object e)
+        {
+        }
+
+        private void MenuFlyout_Closed(object sender, object e)
+        {
+        }
+
+        private void MenuFlyout_Opening(object sender, object e)
+        {
+            menu_click = false;
+            if(circulo || quadrado)
+            {
+                drawingCanvas.Undo_Draw(canvas);
+            }
+        }
+
+        private void canvas_PreviewKeyDown(object sender, KeyRoutedEventArgs e)
+        {
+
+        }
+
+        private void canvas_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+        }
+
+        private void MenuFlyoutItem_Click_2(object sender, RoutedEventArgs e)
+        {
+            linha = false;
+            quadrado = true; // true
+            circulo = false;
+            retangulo = false;
+        }
+
+        private void MenuFlyoutItem_Click_3(object sender, RoutedEventArgs e)
+        {
+            linha = false;
+            quadrado = false;
+            circulo = false;
+            retangulo = true; // true
+        }
+
+        private void MenuFlyoutItem_Click_4(object sender, RoutedEventArgs e)
+        {
+            linha = false;
+            quadrado = false;
+            circulo = true;//true
+            retangulo = false;
+        }
+
         private void click_clear(object sender, RoutedEventArgs e)
         {
 
         }
+
 
         private void full_Screen(bool full)
         {
